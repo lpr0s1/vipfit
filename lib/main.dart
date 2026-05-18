22a1b2ec1d73ad197421b56e8bcccc5fa1b3d14c
lpr0s1 <119435425+lpr0s1@users.noreply.github.com>
2026-05-18 20:21:04 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(home: Scaffold(body: CrashTest())));
+void main() => runApp(const MaterialApp(home: Scaffold(body: BudgetExpress())));
 
-class CrashTest extends StatelessWidget {
-  const CrashTest({super.key});
+class BudgetExpress extends StatefulWidget {
+  const BudgetExpress({super.key});
+  @override
+  State<BudgetExpress> createState() => _BudgetExpressState();
+}
+
+class _BudgetExpressState extends State<BudgetExpress> {
+  final int salaire = 2500;
+  final int depenses = 1500;
+  int get resteAVivre => salaire - depenses;
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('DART EN FORCE 🚀', style: TextStyle(fontSize: 24)));
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Salaire: $salaire €', style: const TextStyle(fontSize: 20)),
+          Text('Dépenses: $depenses €', style: const TextStyle(fontSize: 20)),
+          const SizedBox(height: 20),
+          Text('Reste à vivre: $resteAVivre €', style: const TextStyle(fontSize: 26, fontWeight: FontWeight.bold, color: Colors.cyan)),
+        ],
+      ),
+    );
   }
 }
